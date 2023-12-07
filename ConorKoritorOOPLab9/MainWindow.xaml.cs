6792d1c36ade9731d019a05924b05ba05b6cd9a5
Conor Koritor <ckoritor@gmail.com>
2023-12-07 11:44:22 +0000

    Adding/Removingplayers Logic Added including Spaces Left

--- ConorKoritorOOPLab9/MainWindow.xaml.cs
+++ ConorKoritorOOPLab9/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -30,23 +32,98 @@ namespace ConorKoritorOOPLab9
 
     public partial class MainWindow : Window
     {
+        ObservableCollection<Player> AllPlayers = new ObservableCollection<Player>();
+        ObservableCollection<Player> SelectedPlayers = new ObservableCollection<Player>();
+        string[] firstNames = {
+                "Adam", "Amelia", "Ava", "Chloe", "Conor", "Daniel", "Emily",
+                "Emma", "Grace", "Hannah", "Harry", "Jack", "James",
+                "Lucy", "Luke", "Mia", "Michael", "Noah", "Sean", "Sophie"};
+
+        string[] lastNames = {
+                "Brennan", "Byrne", "Daly", "Doyle", "Dunne", "Fitzgerald", "Kavanagh",
+                "Kelly", "Lynch", "McCarthy", "McDonagh", "Murphy", "Nolan", "O'Brien",
+                "O'Connor", "O'Neill", "O'Reilly", "O'Sullivan", "Ryan", "Walsh"
+            };
+        int SpacesLeft = 11;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            Player player = new Player("Conor", "Koritor", new DateTime(1998, 12, 26), Position.GoalKeeper);
+            CreatePlayers();
 
-            txtblkSpacesLeft.Text = player.ToString();
+            lstbxAllPlayers.ItemsSource = AllPlayers;
+            lstbxSelectedPlayers.ItemsSource = SelectedPlayers;
+            txtblkSpacesLeft.Text = $"{SpacesLeft}";
         }
 
         private void btnAddPlayer_Click(object sender, RoutedEventArgs e)
         {
 
-        }
+            if (SpacesLeft != 0)
+            {
+                if (lstbxAllPlayers.SelectedItem != null)
+                {
+                    SelectedPlayers.Add(lstbxAllPlayers.SelectedItem as Player);
+                    AllPlayers.Remove(lstbxAllPlayers.SelectedItem as Player);
+                    SpacesLeft--;
+                    txtblkSpacesLeft.Text = $"{SpacesLeft}";
+                }
+            }
 
+            
+        }
         private void btnRemovePlayer_Click(object sender, RoutedEventArgs e)
         {
+            if (lstbxSelectedPlayers.SelectedItem != null)
+            {
 
+                AllPlayers.Add(lstbxSelectedPlayers.SelectedItem as Player);
+                SelectedPlayers.Remove(lstbxSelectedPlayers.SelectedItem as Player);
+                SpacesLeft++;
+                txtblkSpacesLeft.Text = $"{SpacesLeft}";
+            }
+        }
+
+        private void CreatePlayers()
+        {
+            for(int i = 0; i < 18; i++)
+            {
+                Player p;
+                if(i < 2)
+                {
+                    p = new Player(firstNames[ThreadSafeRandom.ThisThreadsRandom.Next(0, firstNames.Length)], lastNames[ThreadSafeRandom.ThisThreadsRandom.Next(0, lastNames.Length)], new DateTime(ThreadSafeRandom.ThisThreadsRandom.Next(1993,2004), ThreadSafeRandom.ThisThreadsRandom.Next(1,13), ThreadSafeRandom.ThisThreadsRandom.Next(1, 28)), Position.GoalKeeper);
+                }
+                else if (i < 8)
+                {
+                    p = new Player(firstNames[ThreadSafeRandom.ThisThreadsRandom.Next(0, firstNames.Length)], lastNames[ThreadSafeRandom.ThisThreadsRandom.Next(0, lastNames.Length)], new DateTime(ThreadSafeRandom.ThisThreadsRandom.Next(1993, 2004), ThreadSafeRandom.ThisThreadsRandom.Next(1, 13), ThreadSafeRandom.ThisThreadsRandom.Next(1, 28)), Position.Defender);
+                }
+                else if (i < 14)
+                {
+                    p = new Player(firstNames[ThreadSafeRandom.ThisThreadsRandom.Next(0, firstNames.Length)], lastNames[ThreadSafeRandom.ThisThreadsRandom.Next(0, lastNames.Length)], new DateTime(ThreadSafeRandom.ThisThreadsRandom.Next(1993, 2004), ThreadSafeRandom.ThisThreadsRandom.Next(1, 13), ThreadSafeRandom.ThisThreadsRandom.Next(1, 28)), Position.Midfielder);
+                }
+                else
+                {
+                    p = new Player(firstNames[ThreadSafeRandom.ThisThreadsRandom.Next(0, firstNames.Length)], lastNames[ThreadSafeRandom.ThisThreadsRandom.Next(0, lastNames.Length)], new DateTime(ThreadSafeRandom.ThisThreadsRandom.Next(1993, 2004), ThreadSafeRandom.ThisThreadsRandom.Next(1, 13), ThreadSafeRandom.ThisThreadsRandom.Next(1, 28)), Position.Forward);
+                }
+                
+                AllPlayers.Add(p);
+            }
+        }
+        
+    }
+    public static class ThreadSafeRandom
+    {
+        /*Creates a new random object so that shuffle can randomize the order of the cards
+        * This new random is created with a seed equal to the amount of ticks
+        * the program has run for times 31 plus the ID of the current thread
+        * This increases the randomness compared to a new random object with no seed
+        */
+        [ThreadStatic] private static Random Local;
+
+        public static Random ThisThreadsRandom
+        {
+            get { return Local ?? (Local = new Random(unchecked(Environment.TickCount * 31 + Thread.CurrentThread.ManagedThreadId))); }
         }
     }
 }
